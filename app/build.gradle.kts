c88de696e97b01307063a543706bc3225461f46b
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-09-06 21:22:37 +0800

    Add Room, Retrofit, and OkHttp dependencies; update Gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("kapt")
 }
 
 android {
@@ -56,4 +57,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler) // for annotation processing
+    implementation(libs.androidx.room.ktx) // for coroutines support
+
+    implementation(libs.retrofit2)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.okhttp3.logging.interceptor)
+
 }
