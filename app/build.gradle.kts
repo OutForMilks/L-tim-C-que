ebd7a7a596e9eff3da82675b36817d7ae9385f74
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-11-08 21:50:51 +0800

    adjusted gradle based on the navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -41,7 +42,16 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") //Can delete if no time to implement logging
-    
+
+    //Nav Bar
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
+
+    //Mat component
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
