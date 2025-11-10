ca8b0c51d8fdd931e369bed3b8d3aa38b944b87a
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-11-10 17:05:44 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/l_tim_c_que/MainActivity.kt
+++ app/src/main/java/com/example/l_tim_c_que/MainActivity.kt
@@ -1,159 +1,22 @@
 package com.example.l_tim_c_que
 
-import android.content.Intent
-import android.content.res.ColorStateList
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var homeItem: LinearLayout
-    private lateinit var searchItem: LinearLayout
-    private lateinit var recentsItem: LinearLayout
-    private lateinit var bookmarksItem: LinearLayout
-
-    private lateinit var homeIcon: ImageView
-    private lateinit var searchIcon: ImageView
-    private lateinit var recentsIcon: ImageView
-    private lateinit var bookmarksIcon: ImageView
-
-    private lateinit var homeText: TextView
-    private lateinit var searchText: TextView
-    private lateinit var recentsText: TextView
-    private lateinit var bookmarksText: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        homeItem = findViewById(R.id.home_item)
-        searchItem = findViewById(R.id.search_item)
-        recentsItem = findViewById(R.id.recents_item)
-        bookmarksItem = findViewById(R.id.bookmark_item)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
 
-        homeIcon = findViewById(R.id.home_icon)
-        searchIcon = findViewById(R.id.search_icon)
-        recentsIcon = findViewById(R.id.recents_icon)
-        bookmarksIcon = findViewById(R.id.bookmark_icon)
-
-        homeText = findViewById(R.id.home_text)
-        searchText = findViewById(R.id.search_text)
-        recentsText = findViewById(R.id.recents_text)
-        bookmarksText = findViewById(R.id.bookmark_text)
-
-        updateNavColors(R.id.home_item)
-
-        //recently viewed
-        val recentlyViewedData = RecipeSamples.get()
-        val recentlyViewedAdapter = RecentlyViewedAdapter(recentlyViewedData)
-        val recentlyViewedRecyclerView = findViewById<RecyclerView>(R.id.recently_viewed_recipes)
-        recentlyViewedRecyclerView.adapter = recentlyViewedAdapter
-        recentlyViewedRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        val horizontalSpacing = (18 * resources.displayMetrics.density).toInt()
-        recentlyViewedRecyclerView.addItemDecoration(HorizontalSpacingItemDecoration(horizontalSpacing))
-        recentlyViewedAdapter.setOnItemClickListener { recipe ->
-            val intent = Intent(this, RecipeDetailActivity::class.java)
-
-
-            intent.putExtra("EXTRA_RECIPE", recipe)
-
-            startActivity(intent)
-        }
-
-        recentlyViewedAdapter.setOnBookmarkClickListener { recipe, position ->
-            // database logic here
-            recipe.isBookmarked = !recipe.isBookmarked
-
-            recentlyViewedAdapter.notifyItemChanged(position)
-        }
-
-        //random feature
-        val randomFeatureData = RecipeSamples.get()
-        val randomFeatureAdapter = RandomFeatureAdapter(randomFeatureData)
-        val randomFeatureRecyclerView = findViewById<RecyclerView>(R.id.random_feature_recipes)
-        randomFeatureRecyclerView.adapter = randomFeatureAdapter
-        randomFeatureRecyclerView.layoutManager = GridLayoutManager(this, 2)
-        val spacingInPixels = (22 * resources.displayMetrics.density).toInt()
-        randomFeatureRecyclerView.addItemDecoration(GridSpacingItemDecoration(2, spacingInPixels, false))
-        randomFeatureAdapter.setOnItemClickListener { recipe ->
-            val intent = Intent(this, RecipeDetailActivity::class.java)
-
-            intent.putExtra("EXTRA_RECIPE", recipe)
-
-            startActivity(intent)
-        }
-
-        randomFeatureAdapter.setOnBookmarkClickListener { recipe, position ->
-            // database logic here
-            recipe.isBookmarked = !recipe.isBookmarked
-
-            randomFeatureAdapter.notifyItemChanged(position)
-        }
-
-        searchItem.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
-        }
-
-        recentsItem.setOnClickListener {
-            val intent = Intent(this, RecentsActivity::class.java)
-            startActivity(intent)
-        }
-
-        bookmarksItem.setOnClickListener {
-            val intent = Intent(this, BookmarksActivity::class.java)
-            startActivity(intent)
-        }
-    }
-    private fun updateNavColors(activeItemId: Int) {
-        val activeColor = ContextCompat.getColor(this, R.color.active_yellow) // #E5AF00
-        val inactiveColor = ContextCompat.getColor(this, R.color.inactive_black) // #000000
-
-        homeIcon.imageTintList = ColorStateList.valueOf(inactiveColor)
-        homeText.setTextColor(inactiveColor)
-
-        searchIcon.imageTintList = ColorStateList.valueOf(inactiveColor)
-        searchText.setTextColor(inactiveColor)
-
-        recentsIcon.imageTintList = ColorStateList.valueOf(inactiveColor)
-        recentsText.setTextColor(inactiveColor)
-
-        bookmarksIcon.imageTintList = ColorStateList.valueOf(inactiveColor)
-        bookmarksText.setTextColor(inactiveColor)
-
-        when (activeItemId) {
-            R.id.home_item -> {
-                homeIcon.imageTintList = ColorStateList.valueOf(activeColor)
-                homeText.setTextColor(activeColor)
-            }
-            R.id.search_item -> {
-                searchIcon.imageTintList = ColorStateList.valueOf(activeColor)
-                searchText.setTextColor(activeColor)
-            }
-            R.id.recents_item -> {
-                recentsIcon.imageTintList = ColorStateList.valueOf(activeColor)
-                recentsText.setTextColor(activeColor)
-            }
-            R.id.bookmark_item -> {
-                bookmarksIcon.imageTintList = ColorStateList.valueOf(activeColor)
-                bookmarksText.setTextColor(activeColor)
-            }
+        navHostFragment?.navController?.let { navController ->
+            val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            bottomNav.setupWithNavController(navController)
         }
     }
 }
