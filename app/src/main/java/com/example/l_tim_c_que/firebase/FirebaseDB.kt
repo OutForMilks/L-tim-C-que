abf1c33ccacf05d5f8b94dbe7d16ba19a24e5b3b
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-11-20 21:09:31 +0800

    add recent and delete oldest methods in getfirebase (todo: test)

--- app/src/main/java/com/example/l_tim_c_que/firebase/FirebaseDB.kt
+++ app/src/main/java/com/example/l_tim_c_que/firebase/FirebaseDB.kt
@@ -7,13 +7,30 @@ import com.example.l_tim_c_que.api.APIModel
 // import com.example.l_tim_c_que.Recipe
 import com.google.firebase.auth.auth
 import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.AggregateSource
+import com.google.firebase.firestore.Filter
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.firestoreSettings
+import com.google.firebase.firestore.memoryCacheSettings
+import com.google.firebase.firestore.persistentCacheSettings
+import java.util.Date
 
 object FirebaseDB {
     val auth = Firebase.auth
     @SuppressLint("StaticFieldLeak")
     val firestore = Firebase.firestore
+    val settings = firestoreSettings {
+        // Use memory cache
+        setLocalCacheSettings(memoryCacheSettings {})
+        // Use persistent disk cache (default)
+        setLocalCacheSettings(persistentCacheSettings {})
+    }
+    init {
+        firestore.firestoreSettings = settings
+    }
     val currentUser: FirebaseUser?
         get() = auth.currentUser
     fun signIn(onComplete: ((Boolean) -> Unit)? = null) {
@@ -55,6 +72,53 @@ object FirebaseDB {
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
                 onComplete.invoke(false)
+            }
+    }
+
+    fun saveRecent(meal: APIModel.MealDetail, onComplete: (Boolean) -> Unit): Unit {
+        if(currentUser == null) {
+            Log.w(TAG, "No current user. Recently viewed not updated.")
+            onComplete.invoke(false)
+            return
+        }
+
+        val count = firestore.collection("recent").count().get(AggregateSource.SERVER).result.count
+        // check if recent is equal to 4, delete excess if yes
+        if(count >= 4) {
+            val overflow = count - 4
+            deleteOldest(overflow)
+        }
+
+        val recent = hashMapOf(
+            "userID" to currentUser!!.uid,
+            "recipe" to meal,
+            "date" to Timestamp(Date())
+        )
+
+        firestore.collection("recent")
+            .add(recent)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                onComplete.invoke(true)
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+                onComplete.invoke(false)
+            }
+    }
+
+    fun deleteOldest(overflow: Long) {
+        firestore.collection("recent")
+            .orderBy("Timestamp", Query.Direction.ASCENDING)
+            .limit(1)
+            .get()
+            .result
+            .documents
+            .forEach {
+                mapEntry -> {
+                    val id = mapEntry.id
+                    firestore.collection("recent").document(id).delete()
+                }
             }
     }
 }
